21f6cf3ba7ccda79ff5a73332ef583d9c3d2b590
Leonardo_Nunes <leonardokarate95@hotmail.com>
2022-08-17 18:17:38 -0300

    implementa requisição get /talker:id

---
+++ src/MyErro.js
@@ -0,0 +1,10 @@
+class MyErro extends Error {
+  status = "";
+  constructor(status, message) {
+    super();
+    this.status = status;
+    this.message = message;
+  }
+};
+
+module.exports = MyErro;
--- src/test/index.test.js
+++ src/test/index.test.js
@@ -1,13 +1,25 @@
-const { expect } = require('chai');
+const chai = require('chai');
+const { expect } = chai
 const fs = require('fs');
 const sinon = require('sinon');
-const { getAllTalkers } = require("../talker");
+const { getAllTalkers, getOneTalker } = require("../talker");
 const allTalkersMock = require('./mock/talkesMock');
 
-sinon.stub(fs,"readFileSync").returns(allTalkersMock)
+chai.use(require('chai-as-promised'))
 describe("teste o endPoint getAllTalkers", () => {
   it("em caso de suscesso", async () => {
+    sinon.stub(fs,"readFileSync").returns(allTalkersMock)
     const atual = await getAllTalkers()
     expect(atual).to.be.deep.equals(allTalkersMock);
+  })
+})
+
+describe("teste o endPoint getOneTalker", () => {
+  it("em caso de suscesso", async () => {
+    const atual = await getOneTalker("1")
+    expect(atual).to.be.deep.equals(allTalkersMock[0]);
+  });
+  it("em caso de falha", async () => {
+    await expect(getOneTalker("a")).to.be.rejectedWith(Error)
   })
 })
